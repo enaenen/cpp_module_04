5a17fa5c55b4b5bd008f87d2907cfb6645fee90a
SpaceChae <enaenen@naver.com>
2022-09-04 03:04:35 +0900

    ??

--- ex03/Materia.hpp
+++ ex03/Materia.hpp
@@ -1,3 +1,13 @@
 #include "AMateria.hpp"
 
-class Materia : public AMateria {};
+class Materia : public AMateria {
+	Materia( void );
+	Materia( std::string const& type );
+	Materia( const Materia& ref );
+	Materia& operator=( const Materia& ref );
+	~Materia( void );
+	// AMateria( std::string const& type );
+	std::string const& getType() const;  // Returns the materia type
+	virtual AMateria* clone() const = 0;
+	virtual void use( ICharacter& target );
+};
