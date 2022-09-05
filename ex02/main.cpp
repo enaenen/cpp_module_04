c7b9a7e82a0fdc026c96cd11a54a4efebf4c0662
Woo Joo Chae <enaenen@naver.com>
2022-09-06 02:21:18 +0900

    main delete others

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -8,33 +8,17 @@ int main() {
 		std::cout << "==============MANDATORY TEST=============" << std::endl
 				  << std::endl;
 		// const AAnimal* x = new AAnimal();
+		// AAnimal aa;
+
 		const AAnimal* i = new Dog();
 		const AAnimal* j = new Cat();
 
 		std::cout << i->getType() << " " << std::endl;
 		std::cout << j->getType() << " " << std::endl;
-		i->makeSound();  // will output the cat sound!
+		i->makeSound();
 		j->makeSound();
-		// meta->makeSound();
 
-		// delete meta;
 		delete i;
-		delete j;
-
-		std::cout << std::endl;
-	}
-
-	{
-		std::cout << "==============WRONG ANIMAL TEST=============" << std::endl
-				  << std::endl;
-		const WrongAnimal* meta = new WrongAnimal();
-		const WrongAnimal* j = new WrongCat();
-
-		std::cout << j->getType() << " " << std::endl;
-		j->makeSound();
-		meta->makeSound();
-
-		delete meta;
 		delete j;
 
 		std::cout << std::endl;
